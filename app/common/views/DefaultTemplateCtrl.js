9d39f3f82e0449548ae4f2718d20cf3faa3044bb
Alex G <guirguis.a@gmail.com>
2016-04-03 00:41:19 -0700

    Fix email subscribe

--- app/common/views/DefaultTemplateCtrl.js
+++ app/common/views/DefaultTemplateCtrl.js
@@ -6,9 +6,14 @@
         .controller('DefaultTemplateCtrl', DefaultTemplateCtrl);
 
 
-    DefaultTemplateCtrl.$inject = ['$window', '$timeout', 'GlobalUtils'];
-    function DefaultTemplateCtrl($window, $timeout, GlobalUtils) {
-        var vm = this;
+    DefaultTemplateCtrl.$inject = ['ENV', '$window', '$timeout', '$http', '$q', 'GlobalUtils'];
+    function DefaultTemplateCtrl(ENV, $window, $timeout, $http, $q, GlobalUtils) {
+        var vm = this,
+
+            URLS = {
+                POST_SUBSCRIPTION: ENV.apiEndpoint + '/api/v1/subscribe'
+            };
+
         vm.isMobileDevice = GlobalUtils.isMobileDevice();
 
         // ----- TODO: Need to move this back in the view -----------------------------
@@ -23,6 +28,7 @@
 
         // ----- INTERFACE ------------------------------------------------------------
         vm.fightTicketRedirect = fightTicketRedirect;
+        vm.submitSubscribeForm = submitSubscribeForm;
 
 
         // ----- PUBLIC METHODS -------------------------------------------------------
@@ -35,6 +41,79 @@
                 $window.location.href= appUrl;
             });
         }
+
+        function submitSubscribeForm(isValid) {
+            vm.subscribeFormDataLoading = true;
+            vm.subscribeFormSubmitted = true;
+            vm.subscribeFormSuccess = false;
+            vm.subscribeFormResponseReceived = false;
+
+            // Only continue if the form is valid
+            if (!isValid) {
+                vm.subscribeFormDataLoading = false;
+                return;
+            }
+
+            // Make the call to save the info
+            var dataObj = {
+                subscriber : {
+                    fullname : '',
+                    email : vm.subscribeForm_email,
+                    postalCode : vm.subscribeForm_zip,
+                    subscriptionType : 'WEB_BROCHURE_LAUNCH_NOTIFICATION',
+                    roleType: 'DEFENDANT'
+                }
+            };
+
+            vm.formName = "subscribe";
+
+            postSubscriberData(dataObj);
+        }
+
+        function postSubscriberData(dataObj) {
+            $http.post(URLS.POST_SUBSCRIPTION, dataObj)
+                .then(
+                function (response) {
+                    if (vm.formName === "contact") {
+                        vm.launchFormResponseReceived = true;
+                        vm.launchFormSuccess = true;
+                        vm.dataLoading = false;
+
+                        // clear form
+                        vm.submitted = false;
+                        vm.fullname = '';
+                        vm.email = '';
+                        vm.zipcode = '';
+                    } else {
+                        vm.subscribeFormResponseReceived = true;
+                        vm.subscribeFormSuccess = true;
+                        vm.subscribeFormDataLoading = false;
+                        vm.subscribeFormSubmitted = false;
+                        vm.subscribeForm_email = '';
+                        vm.subscribeForm_zip = '';
+                    }
+                },
+                function (error) {
+                    if (vm.formName === "contact") {
+                        vm.launchFormResponseReceived = true;
+                        vm.launchFormSuccess = false;
+                        vm.dataLoading = false;
+                    } else {
+                        vm.subscribeFormResponseReceived = true;
+                        vm.subscribeFormSuccess = false;
+                        vm.subscribeFormDataLoading = false;
+                    }
+                    if (error.data && error.data.error) {
+                        vm.subscribeErrorMsg = error.data.error.uiErrorMsg;
+                        return $q.reject(error.data.error.uiErrorMsg);
+                    } else {
+                        vm.subscribeErrorMsg = 'Bummer! Something strange is going on and we were not able to save your info. Please try again or let us know if this keeps happening!';
+                        return $q.reject(vm.subscribeErrorMsg);
+                    }
+                }
+            )
+        }
+
     }
 
 
--- app/pages/home/HomeCtrl.js
+++ app/pages/home/HomeCtrl.js
@@ -13,14 +13,11 @@
             isMobileDevice = GlobalUtils.isMobileDevice(),
 
             URLS = {
-                POST_SUBSCRIPTION: ENV.apiEndpoint + '/api/v1/subscribe',
                 POST_LAWYER_LEAD: ENV.apiEndpoint + '/api/v1/lawyers/lead'
-
             };
 
         // ----- INTERFACE ------------------------------------------------------------
-        vm.saveContactInfo = saveContactInfo;
-        vm.submitSubscribeForm = submitSubscribeForm;
+        //vm.saveContactInfo = saveContactInfo;
         vm.openLawyerFormModal = openLawyerFormModal;
         vm.fightTicketRedirect = fightTicketRedirect;
 
@@ -61,110 +58,37 @@
             );
         }
 
-
-
-
-        // This is for drivers/defendants.
-
-        function saveContactInfo(isValid) {
-            vm.dataLoading = true;
-            vm.submitted = true;
-            vm.launchFormSuccess = false;
-            vm.launchFormResponseReceived = false;
-
-            // Only continue if the login form is valid
-            if (!isValid) {
-                vm.dataLoading = false;
-                return;
-            }
-
-            // Make the call to save the info
-            var dataObj = {
-                subscriber : {
-                    fullName : vm.fullName,
-                    email : vm.email,
-                    postalCode : vm.zipcode,
-                    subscriptionType : 'WEB_BROCHURE_LAUNCH_NOTIFICATION',
-                    roleType: 'DEFENDANT'
-                }
-            };
-			
-			vm.formName = "contact";
-			
-			postSubscriberData(dataObj);
-        }
+        //function saveContactInfo(isValid) {
+        //    vm.dataLoading = true;
+        //    vm.submitted = true;
+        //    vm.launchFormSuccess = false;
+        //    vm.launchFormResponseReceived = false;
+        //
+        //    // Only continue if the login form is valid
+        //    if (!isValid) {
+        //        vm.dataLoading = false;
+        //        return;
+        //    }
+        //
+        //    // Make the call to save the info
+        //    var dataObj = {
+        //        subscriber : {
+        //            fullName : vm.fullName,
+        //            email : vm.email,
+        //            postalCode : vm.zipcode,
+        //            subscriptionType : 'WEB_BROCHURE_LAUNCH_NOTIFICATION',
+        //            roleType: 'DEFENDANT'
+        //        }
+        //    };
+			//
+			//vm.formName = "contact";
+			//
+			//postSubscriberData(dataObj);
+        //}
 		
-        function submitSubscribeForm(isValid) {
-            vm.subscribeFormDataLoading = true;
-            vm.subscribeFormSubmitted = true;
-            vm.subscribeFormSuccess = false;
-            vm.subscribeFormResponseReceived = false;
 
-            // Only continue if the form is valid
-            if (!isValid) {
-                vm.subscribeFormDataLoading = false;
-                return;
-            }
-
-            // Make the call to save the info
-            var dataObj = {
-                subscriber : {
-                    fullname : '',
-                    email : vm.subscribeForm_email,
-                    postalCode : vm.subscribeForm_zip,
-                    subscriptionType : 'WEB_BROCHURE_LAUNCH_NOTIFICATION',
-                    roleType: 'DEFENDANT'
-                }
-            };
-			
-			vm.formName = "subscribe";
-			
-			postSubscriberData(dataObj);
-        }
 		
-		function postSubscriberData(dataObj) {
-            $http.post(URLS.POST_SUBSCRIPTION, dataObj)
-                .then(
-                    function (response) {
-						if (vm.formName === "contact") {
-							vm.launchFormResponseReceived = true;
-							vm.launchFormSuccess = true;
-							vm.dataLoading = false;
 
-							// clear form
-							vm.submitted = false;
-							vm.fullname = '';
-							vm.email = '';
-							vm.zipcode = '';
-						} else { 
-							vm.subscribeFormResponseReceived = true;
-							vm.subscribeFormSuccess = true;
-							vm.subscribeFormDataLoading = false;
-							vm.subscribeFormSubmitted = false;
-							vm.subscribeForm_email = '';
-							vm.subscribeForm_zip = '';
-						}
-                    },
-                    function (error) {
-						if (vm.formName === "contact") {
-							vm.launchFormResponseReceived = true;
-							vm.launchFormSuccess = false;
-							vm.dataLoading = false;
-						} else {
-							vm.subscribeFormResponseReceived = true;
-							vm.subscribeFormSuccess = false;
-							vm.subscribeFormDataLoading = false;
-						}
-                        if (error.data && error.data.error) {
-                            vm.subscribeErrorMsg = error.data.error.uiErrorMsg;
-                            return $q.reject(error.data.error.uiErrorMsg);
-                        } else {
-                            vm.subscribeErrorMsg = 'Bummer! Something strange is going on and we were not able to save your info. Please try again or let us know if this keeps happening!';
-                            return $q.reject(vm.subscribeErrorMsg);
-                        }
-                    }
-                )
-		}
     }
 
 
