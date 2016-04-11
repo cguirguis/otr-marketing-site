c32e594f053c02382871d2b266039fd61f80425d
Chris Guirguis <cguirguis@gmail.com>
2016-04-10 17:16:20 -0700

    Merge site.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,31 @@
+// Include gulp
 var gulp = require('gulp');
+
+// Include plugins
 var gutil = require('gulp-util');
 var sass = require('gulp-ruby-sass');
+var concat = require('gulp-concat');
+
 
 gulp.task('sass', function () {
-    return sass('./assets/scss/site.scss')
+    return sass('./assets/scss/*.scss')
         .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('watch', function() {
-    // Watches for changes in style.sass and runs the sass task
-    gulp.watch('./assets/scss/site.scss', ['sass'])
+// Concatenate CSS Files
+gulp.task('concat-css', function() {
+    return gulp.src([
+            'assets/css/site.css',
+            'assets/css/state-page.css',
+            'assets/css/fight-page.css'
+        ])
+        .pipe(concat('site.css'))
+        .pipe(gulp.dest('assets/css'));
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('watch', function() {
+    // Watches for changes in sass/ and runs the sass task
+    gulp.watch('./assets/scss/*.scss', ['sass', 'concat-css'])
+});
+
+gulp.task('default', ['sass', 'concat-css', 'watch']);
