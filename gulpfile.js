c07db58c11b98bae33f6bd7aa154e2bc9570ae96
Chris Guirguis <cguirguis@gmail.com>
2016-04-10 22:00:19 -0700

    Fix state links, add gulp task to clean up css files

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var sass = require('gulp-ruby-sass');
 var concat = require('gulp-concat');
+var del = require('del');
 
 gulp.task('sass', function () {
     return sass('./assets/scss/*.scss')
         .pipe(gulp.dest('./assets/css'));
 });
 
-// Concatenate CSS Files
 gulp.task('concat-css', ['sass'], function() {
     return gulp.src([
             'assets/css/site.css',
@@ -22,9 +22,16 @@ gulp.task('concat-css', ['sass'], function() {
         .pipe(gulp.dest('assets/css'));
 });
 
-gulp.task('watch', function() {
-    // Watches for changes in sass/ and runs the sass task
-    gulp.watch('./assets/scss/*.scss', ['sass', 'concat-css'])
+gulp.task('clean', ['concat-css'], function() {
+    return del([
+        'assets/css/state-page.css',
+        'assets/css/fight-page.css'
+    ]);
 });
 
-gulp.task('default', ['concat-css']);
+gulp.task('watch', function() {
+    // Watches for scss file changes
+    gulp.watch('./assets/scss/*.scss', ['clean'])
+});
+
+gulp.task('default', ['clean']);
