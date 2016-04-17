3130754f027c8e125dd4c278f59994cbbd763ab0
Chris Guirguis <cguirguis@gmail.com>
2016-04-16 19:43:16 -0700

    State content updates, expandable sections

--- app/directives/expandableSection.directive.js
+++ app/directives/expandableSection.directive.js
@@ -1,3 +1,25 @@
-/**
- * Created by Chris on 4/16/16.
- */
+'use strict';
+
+angular.module("brochure").directive('expandableSection', function() {
+    return function(scope, element, attrs) {
+
+        var initialHeight = $(element).css("height");
+
+        // Attach click handler on section's child h3 element
+        $(element).children("h3:first-child").on("click", function(event) {
+            var contentSection = $(event.target).parent();
+            contentSection.toggleClass("collapsed");
+
+            var collapse = contentSection.hasClass("collapsed");
+            if (collapse) {
+                contentSection.animate({
+                    height: "55px"
+                }, 200, function () {});
+            } else {
+                contentSection.animate({
+                    height: initialHeight
+                }, 200, function () {});
+            }
+        });
+    };
+});
