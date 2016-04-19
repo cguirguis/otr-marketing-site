9ab96e4a76ed2adb0115b8fed3b801c5294b3d3b
Chris Guirguis <cguirguis@gmail.com>
2016-04-18 22:58:23 -0700

    Fix scroll issue and image sizing on state pages

--- app/app.js
+++ app/app.js
@@ -98,7 +98,7 @@
 
         $provide.decorator('$uiViewScroll', function ($delegate) {
             return function (uiViewElement) {
-                var top = uiViewElement[0].getBoundingClientRect().top;
+                //var top = uiViewElement[0].getBoundingClientRect().top;
                 window.scrollTo(0, (top - 30));
                 // Or some other custom behaviour...
             };
