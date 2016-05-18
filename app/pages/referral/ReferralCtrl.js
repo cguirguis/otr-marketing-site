c978112c60d8ab0a2ce8a8aa4064112ae81851b3
Chris Guirguis <cguirguis@gmail.com>
2016-05-18 01:21:59 -0700

    Add signed out version of Referral page

--- app/pages/referral/ReferralCtrl.js
+++ app/pages/referral/ReferralCtrl.js
@@ -10,7 +10,8 @@
     function ReferralCtrl($stateParams) {
         var vm = this;
 
-        vm.referralLink = "http://fight.offtherecord.com/" + $stateParams.referralCode;
+        vm.referralCode = $stateParams.referralCode;
+        vm.referralLink = "http://fight.offtherecord.com/" + vm.referralCode;
         var encodedReferralLink = encodeURIComponent(vm.referralLink);
 
         // ----- INTERFACE ------------------------------------------------------------
@@ -29,9 +30,7 @@
             try {
                 var successful = document.execCommand('copy');
                 var msg = successful ? 'successful' : 'unsuccessful';
-                console.log('Copying text command was ' + msg);
             } catch (err) {
-                console.log('Oops, unable to copy');
             }
         }
 
@@ -44,7 +43,6 @@
         }
 
         function shareLinkOnFacebook() {
-            console.log(encodedReferralLink);
             var fbShareUrl = "https://www.facebook.com/v2.5/dialog/share?app_id=545669822241752" +
                 "&caption=OFFTHERECORD.COM" +
                 "&description=Off%20the%20Record%20is%20the%20smart%2C%20easy%20way%20to%20fight%20your%20speeding%20tickets%20and%20moving%20violations.%20We%20win%20or%20its%20free!%20Sign%20up%20now%20to%20claim%20%2420%20credit%20on%20your%20first%20ticket.&amp;display=popup&amp;e2e=%7B%7D" +
